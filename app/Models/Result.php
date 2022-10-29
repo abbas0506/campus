d926d52ced3dcec61552611e868d174897ad093f
abbas0506 <abbas.sscs@gmail.com>
2022-10-29 22:41:28 +0500

    291022

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Result extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'student_id',
+        'semester_id',
+        // 'program_id',
+        // 'shift_id',
+        // 'section',
+        'course_id',
+        'is_reapper',
+        'assignment',
+        'presentation',
+        'midterm',
+        'summative',
+        'teacher_id',
+        'internal_id',
+        'hod_id',
+        'forwarded_at',
+        'kpo_id',
+        'controller_id',
+        'approved_at',
+
+    ];
 }
