13a208e62edcd1ae2b349de6b6941bea673f1979
mianazeemdaula <mianazeemdaula@gmail.com>
2022-12-15 19:30:24 +0500

    bas

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -25,80 +25,80 @@ class UserSeeder extends Seeder
             'department_id' => 1,
 
         ]);
-        $user->assignRole('teacher', 'admin', 'controller');
+        $user->assignRole('admin', 'controller');
 
-        $user = User::create([
-            'name' => 'Prof. Rashid Hussain',
-            'email' => 'hod1@uo.edu.pk',
-            'password' => Hash::make('password'),
-            'department_id' => 1,
+        // $user = User::create([
+        //     'name' => 'Prof. Rashid Hussain',
+        //     'email' => 'hod1@uo.edu.pk',
+        //     'password' => Hash::make('password'),
+        //     'department_id' => 1,
 
-        ]);
-        $user->assignRole(['teacher']);
+        // ]);
+        // $user->assignRole(['teacher']);
 
-        $user = User::create([
-            'name' => 'Prof. Nasir Mehmood',
-            'email' => 'hod2@uo.edu.pk',
-            'password' => Hash::make('password'),
-            'department_id' => 1,
+        // $user = User::create([
+        //     'name' => 'Prof. Nasir Mehmood',
+        //     'email' => 'hod2@uo.edu.pk',
+        //     'password' => Hash::make('password'),
+        //     'department_id' => 1,
 
-        ]);
+        // ]);
 
-        $user->assignRole(['teacher']);
+        // $user->assignRole(['teacher']);
 
 
-        $user = User::create([
-            'name' => 'Mr. Ahmad Ali',
-            'email' => 'teacher1@uo.edu.pk',
-            'password' => Hash::make('password'),
-            'department_id' => 1,
+        // $user = User::create([
+        //     'name' => 'Mr. Ahmad Ali',
+        //     'email' => 'teacher1@uo.edu.pk',
+        //     'password' => Hash::make('password'),
+        //     'department_id' => 1,
 
-        ]);
-        $user->assignRole('teacher');
+        // ]);
+        // $user->assignRole('teacher');
 
-        $user = User::create([
-            'name' => 'Prof. Muhammad Ahmad',
-            'email' => 'teacher2@uo.edu.pk',
-            'password' => Hash::make('password'),
-            'department_id' => 1,
+        // $user = User::create([
+        //     'name' => 'Prof. Muhammad Ahmad',
+        //     'email' => 'teacher2@uo.edu.pk',
+        //     'password' => Hash::make('password'),
+        //     'department_id' => 1,
 
-        ]);
-        $user->assignRole('teacher');
+        // ]);
+        // $user->assignRole('teacher');
 
-        $user = User::create([
-            'name' => 'Mr. Muhammad Ejaz',
-            'email' => 'teacher3@uo.edu.pk',
-            'password' => Hash::make('password'),
-            'department_id' => 1,
+        // $user = User::create([
+        //     'name' => 'Mr. Muhammad Ejaz',
+        //     'email' => 'teacher3@uo.edu.pk',
+        //     'password' => Hash::make('password'),
+        //     'department_id' => 1,
 
-        ]);
-        $user->assignRole('teacher');
+        // ]);
+        // $user->assignRole('teacher');
 
-        $user = User::create([
-            'name' => 'Assistant Prof. Muhammad Luqman',
-            'email' => 'teacher4@uo.edu.pk',
-            'password' => Hash::make('password'),
-            'department_id' => 1,
+        // $user = User::create([
+        //     'name' => 'Assistant Prof. Muhammad Luqman',
+        //     'email' => 'teacher4@uo.edu.pk',
+        //     'password' => Hash::make('password'),
+        //     'department_id' => 1,
 
-        ]);
-        $user->assignRole('teacher');
+        // ]);
+        // $user->assignRole('teacher');
 
-        $user = User::create([
-            'name' => 'Prof. Shabbir Hussain',
-            'email' => 'teacher5@uo.edu.pk',
-            'password' => Hash::make('password'),
-            'department_id' => 2,
+        // $user = User::create([
+        //     'name' => 'Prof. Shabbir Hussain',
+        //     'email' => 'teacher5@uo.edu.pk',
+        //     'password' => Hash::make('password'),
+        //     'department_id' => 2,
 
-        ]);
-        $user->assignRole('teacher');
+        // ]);
+        // $user->assignRole('teacher');
 
-        $user = User::create([
-            'name' => 'Prof. Nazir Hussain',
-            'email' => 'teacher6@uo.edu.pk',
-            'password' => Hash::make('password'),
-            'department_id' => 2,
+        // $user = User::create([
+        //     'name' => 'Prof. Nazir Hussain',
+        //     'email' => 'teacher6@uo.edu.pk',
+        //     'password' => Hash::make('password'),
+        //     'department_id' => 2,
 
-        ]);
-        $user->assignRole('teacher');
+        // ]);
+        // $user->assignRole('teacher');
     }
 }
