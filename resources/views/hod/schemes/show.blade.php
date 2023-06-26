d853ce267303722b25debbbc7ecbfc45801a07af
abbas0506 <abbas.sscs@gmail.com>
2023-06-26 14:33:21 +0500

    26062300

--- resources/views/hod/schemes/show.blade.php
+++ resources/views/hod/schemes/show.blade.php
@@ -72,6 +72,15 @@
                 </div>
                 @endforeach
 
+                <div class="w-full mt-2">
+                    <a href="{{route('schemes.meta.create', [$scheme->id, $semester_no])}}" class="flex items-center btn-teal text-sm float-left">
+                        Creat Slot
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 ml-2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z" />
+                        </svg>
+                    </a>
+                </div>
+
                 @foreach($scheme->scheme_details->where('semester_no',$semester_no)->where('slot',0) as $scheme_detail)
 
 
@@ -99,14 +108,6 @@
                         </svg>
                     </a>
                 </div> -->
-                <div class="w-full mt-2">
-                    <a href="{{route('schemes.meta.create', [$scheme->id, $semester_no])}}" class="flex items-center btn-teal text-sm float-left">
-                        Creat Slot
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 ml-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z" />
-                        </svg>
-                    </a>
-                </div>
 
             </div>
 
