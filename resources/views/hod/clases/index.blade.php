0092576e255f48445609d8dffb56d8ff50e1384e
abbas0506 <abbas.sscs@gmail.com>
2023-07-04 14:19:20 +0500

    040723

--- resources/views/hod/clases/index.blade.php
+++ resources/views/hod/clases/index.blade.php
@@ -69,7 +69,7 @@
             <div class="head">
                 <h2 class="flex items-center space-x-4">
                     {{$program->short}}
-                    <span class="text-xs ml-4 font-thin">Classes:{{$program->clases()->active()->count()}}</span>
+                    <span class="text-xs ml-4 font-thin">Classes:{{$program->clases()->active(session('semester')->id)->count()}}</span>
                     <span class="text-xs ml-4 font-thin">Sections:{{$program->sections()->count()}}</span>
                     <div class="flex items-center space-x-1">
                         <span class="bx bx-user text-[12px] text-slate-500"></span>
@@ -79,9 +79,10 @@
                 </h2>
                 <i class="bx bx-chevron-down text-lg"></i>
             </div>
+            Session: {{session('semester')->id}}
             <div class="body">
 
-                @foreach($program->clases()->active()->get() as $clas)
+                @foreach($program->clases()->active(session('semester')->id)->get() as $clas)
                 <div class="flex items-center w-full border-b py-1 space-x-4">
                     <div class="flex items-center justify-between w-1/2 md:w-1/4">
                         <div class="text-sm">{{$clas->short()}}</div>
