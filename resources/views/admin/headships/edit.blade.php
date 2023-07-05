98503324ebaffa4257222dfc3d1cc14fee3d7b0c
abbas0506 <abbas.sscs@gmail.com>
2023-07-06 00:12:25 +0500

    05072306

--- resources/views/admin/headships/edit.blade.php
+++ resources/views/admin/headships/edit.blade.php
@@ -1,27 +1,13 @@
 @extends('layouts.admin')
 @section('page-content')
-<h1 class="mt-12"><a href="{{route('departments.index')}}">Deptt & Headship</a></h1>
+<h1><a href="{{route('departments.index')}}">Deptt & Headship</a></h1>
 <div class="bread-crumb">{{$selected_department->name}} / assign hod</div>
 
 <div class="container w-full mx-auto mt-8">
     <div class="flex items-center flex-wrap justify-between">
         <div class="flex relative ">
             <input type="text" id='search_input' placeholder="Search ..." class="search-indigo" oninput="search()">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 absolute right-1 top-3">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-            </svg>
-        </div>
-        <div class="flex relative">
-            <select name="department_id" id="department_filter" class="input-indigo py-1" onchange="search()">
-                <option value="">No filter</option>
-                @foreach($departments as $department)
-                <option value="{{$department->id}}">{{$department->name}}</option>
-                @endforeach
-            </select>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 absolute -right-6 top-2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
-            </svg>
-
+            <i class="bi bi-search absolute right-1 top-3"></i>
         </div>
         <a href="{{route('headships.create')}}" class="btn-indigo text-sm">
             Create & Assign New
@@ -86,42 +72,8 @@
 </table>
 </div>
 <script type="text/javascript">
-    function search() {
-        // var searchtext = event.target.value.toLowerCase();
-        var searchtext = $('#search_input').val().toLowerCase();
-        var filtertext = $('#department_filter option:selected').text().toLowerCase();
-        if (filtertext == 'no filter') {
-            $('.tr').each(function() {
-                //dont filter
-                if (!(
-                        $(this).children().eq(0).prop('outerText').toLowerCase().includes(searchtext)
-                    )) {
-                    $(this).addClass('hidden');
-                } else {
-                    $(this).removeClass('hidden');
-                }
-            });
-        } else {
-            //apply filter
-            $('.tr').each(function() {
-                if (!(
-                        $(this).children().eq(0).prop('outerText').toLowerCase().includes(searchtext) &&
-                        $(this).children().eq(1).prop('outerText').toLowerCase().includes(filtertext)
-                    )) {
-                    $(this).addClass('hidden');
-                } else {
-                    $(this).removeClass('hidden');
-                }
-            });
-        }
-
-
-    }
-
     function assign(formid) {
-
         event.preventDefault();
-
         Swal.fire({
             title: 'Are you sure?',
             text: "You wont to assign HOD!",
