58d85b5edeeb3b239c3cedd16f21b3b137d93d6f
abbas0506 <abbas.sscs@gmail.com>
2023-08-07 18:09:52 +0500

    07082307

--- resources/views/hod/courseplan/index.blade.php
+++ resources/views/hod/courseplan/index.blade.php
@@ -50,7 +50,7 @@
     <div class="collapsible">
         <div class="head">
             <h2 class="flex items-center space-x-4">
-                {{$program->id}}, {{$program->short}}
+                {{$program->short}}
                 <span class="text-xs ml-4 font-thin">Classes:{{$program->clases()->active()->count()}}</span>
             </h2>
             <i class="bx bx-chevron-down text-lg"></i>
