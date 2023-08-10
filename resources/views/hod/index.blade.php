6d3e5d23bc77e8598bfd9c6e0a6ec0c6edae5f7e
abbas0506 <abbas.sscs@gmail.com>
2023-08-10 20:10:26 +0500

    10082302

--- resources/views/hod/index.blade.php
+++ resources/views/hod/index.blade.php
@@ -4,7 +4,7 @@
     <!--welcome  -->
     <div class="flex items-center">
         <div class="flex-1">
-            <div class="h2">Welcome dear {{ Auth::user()->name }}!</div>
+            <h2>Welcome dear {{ Auth::user()->name }}!</h2>
             <div class="bread-crumb">
                 <div>HOD</div>
                 <div>/</div>
