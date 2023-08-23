56962fbbd214a1f46ecfd0c720796e05bd4b23b0
abbas0506 <abbas.sscs@gmail.com>
2023-08-23 14:21:40 +0500

    22082307

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Mail;
 
 use App\Models\User;
 use Exception;
@@ -52,7 +53,18 @@ class AuthController extends Controller
         // echo $request->email;
         try {
             if (Auth::attempt($credentials)) {
-                Auth::user()->sendCode();
+                // Auth::user()->sendCode();
+                Mail::raw('Helo it is msg from admin', function ($message) {
+                    //    $message->from('john@johndoe.com', 'John Doe');
+                    //    $message->sender('john@johndoe.com', 'John Doe');
+                    $message->to('abbas.sscs@gmail.com', 'admin');
+                    //    $message->cc('john@johndoe.com', 'John Doe');
+                    //    $message->bcc('john@johndoe.com', 'John Doe');
+                    //    $message->replyTo('john@johndoe.com', 'John Doe');
+                    $message->subject('Code');
+                    //    $message->priority(3);
+                    //    $message->attach('pathToFile');
+                });
                 return redirect('/');
             } else {
                 return redirect()->back()->withErrors(['auth' => 'User credentials incorrect !']);
