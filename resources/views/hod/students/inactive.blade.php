a3d8a04e17a43f113398aa87636bd3d74e591454
abbas0506 <abbas.sscs@gmail.com>
2024-06-15 15:10:16 +0500

    15062401

--- resources/views/hod/students/inactive.blade.php
+++ resources/views/hod/students/inactive.blade.php
@@ -11,7 +11,6 @@
         <div>Inactive</div>
     </div>
 
-    <h2 class="text-red-600 mt-5 "> <i class="bi-person-fill-slash mr-2"></i>Inactive Students: {{$department->currentStudents()->inactive()->count()}}</h2>
     <!-- search -->
     <div class="flex relative w-full md:w-1/3 mt-5">
         <input type="text" id='searchby' placeholder="Search ..." class="search-indigo w-full" oninput="search(event)">
